7dd8a0c39f1f62c640375269e00a4166b9daa8fe
Alexander Lee <alexanderlee@alexanders-mbp.lan>
2019-03-25 15:39:34 -0500

    changed var to let

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,12 @@
 // Typewriting effect
-var i = 0,
+let i = 0,
   txt = 'Caring for you at the comfort of your own home.',
   speed = 40;
 
 function typeWriter() {
   if (i < txt.length) {
-  document.getElementById('typewriter').innerHTML += txt.charAt(i);
-  i++;
-  setTimeout(typeWriter, speed);
+    document.getElementById('typewriter').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
 }
